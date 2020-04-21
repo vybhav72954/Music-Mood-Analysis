1f03763df43ffd4580533c0973117de873942d67
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-04-21 18:21:25 +0530

    Converter_Updated
    Follow the instructions in the comment

--- Converter.py
+++ Converter.py
@@ -1,12 +1,23 @@
+# FOLLOW THESE INSTRUCTIONS CAREFULLY
+# COPY ALL THE SONGS IN A NEW DIRECTORY CALLED "Song_Data", KEEP BOTH THE DIRECTORIES
+# YOU WIILL HAVE TO CAHNGE USER IN THE PATH, e.g. MY USER IS ASUS, DO THE CAHNGES ACCORDINGLY
+# I SUGGEST YOU MAKE A SEPARATE DIRECTORY FOR CODE AND RESULTS, <YOUR WISH>
+# DO NECCESSARY CHANGES IN "song_dir" variable
+# I REPEAT COPY THE SONGS AND KEEP BOTH THE DIRECTORIES
+# THIS WILL COME HANDY LATER ON
 import os
-import pydub
 from glob import glob
 
-song_dir = 'C:/Users/asus/Desktop/Song_test/*wav'  # Directory of File (Extension is acc. to the file to be converted
+import pydub
+
+song_dir = 'C:/Users/asus/Desktop/Capstone_Code/Song_Data/*.mp3'  # Directory of File (Extension is acc. to file being converted
+# If mp3 file ko wav me convert kr rhe, put mp3 here
+print(song_dir)
 song = glob(song_dir)  # Glob for pattern
 print(song)
 for song in song:
-    mp3_song = os.path.splitext(song)[0] + '.mp3'  # Changing the extension to the target file type
+    mp3_song = os.path.splitext(song)[0] + '.wav'  # Changing the extension to the target file type
     sound = pydub.AudioSegment.from_mp3(song)
-    sound.export(mp3_song, format="mp3")   # Actual Conversion yha pr h using PyDub
+    sound.export(mp3_song, format="wav")  # Actual Conversion yha pr h using PyDub
+    os.remove(song)
 print("Conversion Done")
