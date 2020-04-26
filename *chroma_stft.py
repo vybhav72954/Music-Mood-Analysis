4a09dfacaaec5957eaf88dda415c7a459916f325
thedeadlymist <60223179+thedeadlymist@users.noreply.github.com>
2020-04-26 20:05:57 +0530

    Update *chroma_stft.py

--- *chroma_stft.py
+++ *chroma_stft.py
@@ -7,9 +7,6 @@ song="C:/Users/MADHUKAR/Desktop/Song_Data/2_Hello.wav"
 y, sr = librosa.load(song, offset=30, duration=5)
 print(y)
 
-y, sr = librosa.load(librosa.util.example_audio_file())
-librosa.feature.chroma_stft(y=y, sr=sr)
-
 S = np.abs(librosa.stft(y))
 chroma = librosa.feature.chroma_stft(S=S, sr=sr)
 print(chroma)
