a3dc043b8c0a9d996f03d84e1da99dbfdfc6169b
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-04-27 13:42:10 +0530

    Update New_Directories.py

--- Framework/New_Directories.py
+++ Framework/New_Directories.py
@@ -1,1 +1,18 @@
+import os
+import errno
 
+access_rights = 0o777 # Access right, determines the pemission i.e. read, write etc
+
+output_dir = 'C:/Users/asus/Desktop/Graph/Music/TestSubD' #Directory's path
+
+try:
+    os.makedirs(output_dir, access_rights, exist_ok=True)
+    
+except OSError as exc:
+    if exc.errno != errno.EEXIST:
+        raise
+    print("Creation of the directory %s failed" % output_dir)
+    pass
+
+else:
+    print("Successfully created the directory %s" % output_dir)
