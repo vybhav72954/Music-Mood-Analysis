587d85926367d260dbeb3c2243e433204744bbbe
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-04-27 14:35:35 +0530

    Update Chroma_Tonal.py

--- Features/Chroma_Tonal.py
+++ Features/Chroma_Tonal.py
@@ -1,25 +1,62 @@
-import matplotlib.pyplot as plt
 import librosa.display
-import numpy as np
-import IPython.display as ipd
-import librosa
+import librosa as lr
 import matplotlib.pyplot as plt
+from glob import glob
+import os
+import errno
 
-song="C:/Users/MADHUKAR/Desktop/Song_Data/2_Hello.wav"
+data_dir = "C:/Users/asus/Desktop/Song_test/Split/*.wav"
+audio_files = glob(data_dir)
+len(audio_files)
 
-y, sr = librosa.load(song)
-print(y)
+access_rights = 0o777
+output_dir = 'C:/Users/asus/Desktop/Graph/Music/TestSubD1Tonal'
+output_dir1 = 'C:/Users/asus/Desktop/Graph/Music/TestSubD1Chroma'
 
-y = librosa.effects.harmonic(y)
-tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
-print(tonnetz)
-plt.subplot(2, 1, 1)
-librosa.display.specshow(tonnetz, y_axis='tonnetz')
-plt.colorbar()
-plt.title('Tonal Centroids (Tonnetz)')
-plt.show()
-librosa.display.specshow(librosa.feature.chroma_cqt(y, sr=sr), y_axis='chroma', x_axis='time')
-plt.colorbar()
-plt.title('Chroma')
-plt.tight_layout()
-plt.show()
+try:
+    os.makedirs(output_dir, access_rights, exist_ok=True)
+
+except OSError as exc:
+    if exc.errno != errno.EEXIST:
+        raise
+    print("Creation of the directory %s failed" % output_dir)
+    pass
+
+else:
+    print("Successfully created the directory %s" % output_dir)
+
+try:
+    os.makedirs(output_dir1, access_rights, exist_ok=True)
+
+except OSError as exc:
+    if exc.errno != errno.EEXIST:
+        raise
+    print("Creation of the directory %s failed" % output_dir1)
+    pass
+
+else:
+    print("Successfully created the directory %s" % output_dir1)
+
+
+for file in range(0, len(audio_files), 1):
+    y, sr = librosa.load(audio_files[file])
+    print(y)
+
+    y = librosa.effects.harmonic(y)
+    tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
+    print(tonnetz)
+    plt.subplot(2, 1, 1)
+    librosa.display.specshow(tonnetz, y_axis='tonnetz')
+    plt.colorbar()
+    plt.title('Tonal Centroids (Tonnetz)')
+    plt.savefig(f'{output_dir}/Graph_{file}.png', format="PNG")
+    plt.show()
+
+    librosa.display.specshow(librosa.feature.chroma_cqt(y, sr=sr), y_axis='chroma', x_axis='time')
+    plt.colorbar()
+    plt.title('Chroma')
+    plt.tight_layout()
+    plt.savefig(f'{output_dir1}/Graph_{file}.png', format="PNG")
+    plt.show()
+
+print('Tonal Centroids Done, Pitch Formatter  Done')
