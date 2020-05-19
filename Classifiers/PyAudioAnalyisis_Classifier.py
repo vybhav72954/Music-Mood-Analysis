b78cb376330cdf4d0bde6b16d4ce4e22ca723090
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-05-19 17:16:38 +0530

    Update and rename PyAudioAnalyisis_SVM.py to PyAudioAnalyisis_Classifier.py

--- Classifiers/PyAudioAnalyisis_Classifier.py
+++ Classifiers/PyAudioAnalyisis_Classifier.py
@@ -2,4 +2,6 @@
 # This will generate 3 files, make sure u dont delete them as they will come in handy
 
 from pyAudioAnalysis import audioTrainTest as aT
+
+#For Vedansh
 aT.extract_features_and_train(["C:/Users/asus/Desktop/GTZAN/pop/","C:/Users/asus/Desktop/GTZAN/country/","C:/Users/asus/Desktop/GTZAN/blues/","C:/Users/asus/Desktop/GTZAN/disco/","C:/Users/asus/Desktop/GTZAN/jazz/","C:/Users/asus/Desktop/GTZAN/classical/","C:/Users/asus/Desktop/GTZAN/hiphop/","C:/Users/asus/Desktop/GTZAN/metal/","C:/Users/asus/Desktop/GTZAN/reggae/","C:/Users/asus/Desktop/GTZAN/rock/"], 1.0, 0.5, aT.shortTermWindow, aT.shortTermStep, "svm", "SVM_Linear", True)
