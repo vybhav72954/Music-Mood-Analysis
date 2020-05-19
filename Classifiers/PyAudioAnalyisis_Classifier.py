1329c4c1e386e715a425bf092eac23d8218c4f07
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-05-19 17:22:59 +0530

    Update PyAudioAnalyisis_Classifier.py

--- Classifiers/PyAudioAnalyisis_Classifier.py
+++ Classifiers/PyAudioAnalyisis_Classifier.py
@@ -6,3 +6,6 @@ from pyAudioAnalysis import audioTrainTest as aT
 
 #For Vedansh
 aT.extract_features_and_train(["C:/Users/asus/Desktop/GTZAN/pop/","C:/Users/asus/Desktop/GTZAN/country/","C:/Users/asus/Desktop/GTZAN/blues/","C:/Users/asus/Desktop/GTZAN/disco/","C:/Users/asus/Desktop/GTZAN/jazz/","C:/Users/asus/Desktop/GTZAN/classical/","C:/Users/asus/Desktop/GTZAN/hiphop/","C:/Users/asus/Desktop/GTZAN/metal/","C:/Users/asus/Desktop/GTZAN/reggae/","C:/Users/asus/Desktop/GTZAN/rock/"], 1.0, 0.5, aT.shortTermWindow, aT.shortTermStep, "svm", "SVM_Linear", True)
+
+
+aT.extract_features_and_train(["C:/Users/asus/Desktop/GTZAN/pop/","C:/Users/asus/Desktop/GTZAN/country/","C:/Users/asus/Desktop/GTZAN/blues/","C:/Users/asus/Desktop/GTZAN/disco/","C:/Users/asus/Desktop/GTZAN/jazz/","C:/Users/asus/Desktop/GTZAN/classical/","C:/Users/asus/Desktop/GTZAN/hiphop/","C:/Users/asus/Desktop/GTZAN/metal/","C:/Users/asus/Desktop/GTZAN/reggae/","C:/Users/asus/Desktop/GTZAN/rock/"], 1.25, 0.6, aT.shortTermWindow, aT.shortTermStep, "knn", "knn_k=3", True)
