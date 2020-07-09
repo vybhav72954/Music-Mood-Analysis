bd8fee7c4fafa2fcf1dc9b9130c86e72f3f8497d
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-07-09 15:54:36 +0530

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -1,16 +1,12 @@
 # FOLLOW THESE INSTRUCTIONS CAREFULLY
-# COPY ALL THE SONGS IN A NEW DIRECTORY CALLED "Song_Data", KEEP BOTH THE DIRECTORIES
-# YOU WIILL HAVE TO CAHNGE USER IN THE PATH, e.g. MY USER IS ASUS, DO THE CAHNGES ACCORDINGLY
-# I SUGGEST YOU MAKE A SEPARATE DIRECTORY FOR CODE AND RESULTS, <YOUR WISH>
-# DO NECCESSARY CHANGES IN "song_dir" variable
-# I REPEAT COPY THE SONGS AND KEEP BOTH THE DIRECTORIES
+# 
 # THIS WILL COME HANDY LATER ON
 import os
 from glob import glob
 
 import pydub
 
-song_dir = 'C:/Users/asus/Desktop/Capstone_Code/Song_Data/*.mp3'  # Directory of File (Extension is acc. to file being converted
+song_dir = 'C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\*.mp3'  # Directory of File (Extension is acc. to file being converted
 # If mp3 file ko wav me convert kr rhe, put mp3 here
 print(song_dir)
 song = glob(song_dir)  # Glob for pattern
@@ -20,5 +16,4 @@ for song in song:
     sound = pydub.AudioSegment.from_mp3(song)
     sound.export(mp3_song, format="wav")  # Actual Conversion yha pr h using PyDub
     os.remove(song)
-    
 print("Conversion Done")
