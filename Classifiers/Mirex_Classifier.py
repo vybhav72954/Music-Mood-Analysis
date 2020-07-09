8334c4fe112ebb9955e0328e60b005c31e0c4f97
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-07-09 16:42:13 +0530

    Update Mirex_Classifier.py

--- Classifiers/Mirex_Classifier.py
+++ Classifiers/Mirex_Classifier.py
@@ -1,33 +1,42 @@
+#Change the Path
+#Chnage the Model Type
+#Follow  the same naming Convention
+#LOGS
+#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS#LOGS
+
+
 from pyAudioAnalysis import audioTrainTest as aT
-aT.extract_features_and_train(["C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Agressive/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Boisterous/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Rowdy/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Volatile/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Confident/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Fiery/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Intense/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Passionate/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Rousing/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 3/Autumnal/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 3/Brooding/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 3/Literate/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 4/Bittersweet/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 4/Poignant/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 4/Wistful/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 5/Campy/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 5/whimsical/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 5/Wry/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 6/Cheerful/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 6/Rollicking/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Fun/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Humorous/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Silly/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Witty/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 8/Amiable-good natured/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 8/Sweet/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 9/Tense - Anxious/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 9/Visceral/"],
-                              1, 0.25, aT.shortTermWindow, aT.shortTermStep, "randomforest", "RF_Mirex", True)
+midTerm = 3
+shortTerm = 0.0625
+aT.extract_features_and_train(["C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 1\\Agressive\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 1\\Boisterous\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 1\\Rowdy\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 1\\Volatile\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 2\\Confident\\"
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 2\\Fiery\\"
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 2\\Intense\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 2\\Passionate\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 2\\Rousing\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 3\\Autumnal\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 3\\Brooding\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 3\\Literate\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 4\\Bittersweet\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 4\\Poignant\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 4\\Wistful\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 5\\Campy\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 5\\whimsical\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 5\\Wry\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 6\\Cheerful\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 6\\Rollicking\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 7\\Fun\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 7\\Humorous\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 7\\Silly\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 7\\Witty\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 8\\Amiable-good natured\\",
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 8\\Sweet\\"
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 9\\Tense - Anxious\\"
+                               "C:\\Users\\asus\\Desktop\\MIREX\\dataset\\Audio\\Cluster 9\\Visceral\\"],
+                              midTerm, midTerm/4, shortTerm, shortTerm/4, "extratrees", "ET_Vybhav_9-July", True)
 print("******************************************************************************************************************************")
 print("******************************************************************************************************************************")
 print("******************************************************************************************************************************")
@@ -41,50 +50,3 @@ print("*************************************************************************
 print("******************************************************************************************************************************")
 print("******************************************************************************************************************************")
 
-
-
-
-
-
-
-aT.extract_features_and_train(["C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Agressive/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Boisterous/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Rowdy/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 1/Volatile/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Confident/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Fiery/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Intense/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Passionate/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 2/Rousing/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 3/Autumnal/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 3/Brooding/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 3/Literate/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 4/Bittersweet/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 4/Poignant/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 4/Wistful/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 5/Campy/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 5/whimsical/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 5/Wry/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 6/Cheerful/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 6/Rollicking/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Fun/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Humorous/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Silly/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 7/Witty/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 8/Amiable-good natured/",
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 8/Sweet/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 9/Tense - Anxious/"
-                               "C:/Users/asus/Desktop/MIREX-like_mood/Audio/Cluster 9/Visceral/"],
-                              1, 0.25, aT.shortTermWindow, aT.shortTermStep, "gradientboosting", "GB_Mirex", True)
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
-print("******************************************************************************************************************************")
