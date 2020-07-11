6d1b4ddedd61d3319941e5893310f1cc7020ec2a
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-07-11 15:11:22 +0530

    Update ECG_v1.ino

--- BioFeed/ECG/ECG_v1.ino
+++ BioFeed/ECG/ECG_v1.ino
@@ -1,9 +1,9 @@
 
 void setup() {
-// initialize the serial communication:
+
 Serial.begin(9600);
-pinMode(10, INPUT); // Setup for leads off detection LO +
-pinMode(11, INPUT); // Setup for leads off detection LO -
+pinMode(10, INPUT); //  LO +
+pinMode(11, INPUT); //  LO -
  
 }
  
@@ -13,9 +13,9 @@ if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
 Serial.println('!');
 }
 else{
-// send the value of analog input 0:
+// analog input 0:
 Serial.println(analogRead(A0));
 }
-//Wait for a bit to keep serial data from saturating
+//Wait
 delay(1);
 }
