dfe9ca33911655ce0bab7549a43b163ee5b1f7df
armanbeer <52580830+armanbeer@users.noreply.github.com>
2020-07-13 13:53:19 +0530

    Update Random_forest_Files_Classified.py

--- Classifiers/RawData_Classifier/Random_forest_Files_Classified.py
+++ Classifiers/RawData_Classifier/Random_forest_Files_Classified.py
@@ -2,51 +2,29 @@ from pyAudioAnalysis import audioTrainTest as aT
 import os
 import shutil
 
-inp_dir = "D:/Capstone/Testing/Single Song/"
-for path in os.listdir(inp_dir):
-    source = os.path.join(inp_dir, path)
+
+'''
+# For Single File
+class_id, probability, classes = aT.file_classification("D:\\Capstone\\Testing\\Single Song\\Passenger - Let Her Go 🎵.mp3",
+                                                        "C:\\Users\\arman\\PycharmProjects\\Capstone\\Features\\RF_test", "randomforest")
+print(class_id, probability, classes)
+print(classes[int(class_id)])
+'''
+
+d = "D:\\Capstone\\Testing\\Single Song\\" # downsample done songs directory, I suggest make a new copy of that and then run
+for path in os.listdir(d):
+    source = os.path.join(d, path)
     #if os.path.isfile(source):
     print(source)
 
     class_id, probability, classes = aT.file_classification(source,
-                           "C:/Users/arman/PycharmProjects/Capstone/Features/RF_Mirex", "randomforest")
+                                                            "C:\\Users\\arman\\PycharmProjects\\Capstone\\Features\\RF_test", "randomforest")
 
     print(class_id,classes)
-
-    test_loc = {}
-
-    test_loc[0] = 'Agressive'
-    test_loc[1] = 'Boisterous'
-    test_loc[2] = 'Rowdy'
-    test_loc[3] = 'Volatile'
-    test_loc[4] = 'Confident'
-    test_loc[5] = 'Fiery'
-    test_loc[6] = 'Intense'
-    test_loc[7] = 'Passionate'
-    test_loc[8] = 'Rousing'
-    test_loc[9] = 'Autumnal'
-    test_loc[10] = 'Brooding'
-    test_loc[11] = 'Literate'
-    test_loc[12] = 'Bittersweet'
-    test_loc[13] = 'Poignant'
-    test_loc[14] = 'Wistful'
-    test_loc[15] = 'Campy'
-    test_loc[16] = 'Whimsical'
-    test_loc[17] = 'Wry'
-    test_loc[18] = 'Cheerful'
-    test_loc[19] = 'Rollicking'
-    test_loc[20] = 'Fun'
-    test_loc[21] = 'Humorous'
-    test_loc[22] = 'Silly'
-    test_loc[23] = 'Witty'
-    test_loc[24] = 'Amiable-good-natured'
-    test_loc[25] = 'Sweet'
-    test_loc[26] = 'Tense-Anxious'
-    test_loc[27] = 'Visceral'
-    for key in test_loc:
-            if (key == class_id):
-                print(test_loc[key]) # PRINT OUT THE VALUE ACCORDING TO KEY
-                output_dir = "D:/Capstone/Testing/randomForest/" + test_loc[key]
+    for count,name in enumerate(classes):
+            if (count == int(class_id)):
+                print(name) # PRINT OUT THE VALUE ACCORDING TO KEY
+                output_dir = "D:\\Capstone\\Single Song\\" + name # make a new directory where songs in their emotions can be saved
                 print(output_dir) # PRINT OUT THE DIRECTORY ACCORDING TO VALUE
                 ################### EXCEPTION HANDLING ###############
                 try:
@@ -59,6 +37,7 @@ for path in os.listdir(inp_dir):
 
                 else:
                     print("Successfully created the directory %s" % output_dir)
-                #output_dir = output_dir + "/" + str(count) + ".wav" # to use in copyfile function
+                #output_dir = output_dir + "\\" + str(count) + ".wav" # to use in copyfile function
                 shutil.move(source,output_dir)
+
 
