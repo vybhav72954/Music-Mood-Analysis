1e88ac856a202921921148c2019781f487d92cbe
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-07-22 05:15:12 +0530

    Update euclidean_dist.py

--- Framework/euclidean_dist.py
+++ Framework/euclidean_dist.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import csv
 
+import scipy
+from scipy import spatial
+
 mood = pd.read_csv("C:/Users/asus/Desktop/Mood_Coordinates.csv")
 song_value = pd.read_csv("C:/Users/asus/Desktop/Harmonic_AV_test.csv")
-
+'''
 df_temp = mood.loc[:, ['Arousal','Valence']]
 #print (song_value)
 df_temp1 = song_value.loc[:, ['Arousal','Valence']]
@@ -12,11 +15,11 @@ df_temp1 = song_value.loc[:, ['Arousal','Valence']]
 df_n1 = mood.loc[:, ['Arousal','Valence']].to_numpy()
 df_n2 = song_value.loc[:, ['Arousal','Valence']].to_numpy()
 
-'''
+
 for x,y in df_n2:
     for a,b in df_n1:
         np.linalg.norm(df_n2[x][y] - df_n1[a][b])
-'''
+
 
 import scipy
 from scipy import spatial
@@ -24,3 +27,22 @@ ary = scipy.spatial.distance.cdist(mood.loc[:, ['Arousal','Valence']], song_valu
 #print (ary)
 euclid = pd.DataFrame(ary)
 #print (euclid)
+'''
+
+pairs_1 = list(tuple(zip(mood.Arousal, mood.Valence)))
+pairs_2 = list(tuple(zip(song_value.Arousal, song_value.Valence)))
+
+min_distances = []
+closest_pairs = []
+names = []
+for i in pairs_2:
+    min_dist = scipy.spatial.distance.cdist([i], pairs_1, metric='euclidean').min()
+    index_min = scipy.spatial.distance.cdist([i], pairs_1, metric='euclidean').argmin()
+    min_distances.append(min_dist)
+    closest_pairs.append(mood.loc[index_min, ['Arousal', 'Valence']])
+    names.append(mood.loc[index_min, 'Name'])
+song_value['min_distance'] = min_distances
+song_value['closest_pairs'] = [tuple(i.values) for i in closest_pairs]
+song_value['name'] = names
+
+print(song_value)
